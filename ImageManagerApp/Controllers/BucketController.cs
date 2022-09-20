1d59b4cd21fdadcbe42e3e1a8aa61e4542f0b8e7
prince98988 <18bce183@nirmauni.ac.in>
2022-09-20 18:03:30 +0530

    initial commit

---
+++ ImageManagerApp/Controllers/BucketController.cs
@@ -0,0 +1,44 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageManagerApp.Controllers
+{
+    public class BucketController : Controller
+    {
+        private readonly IAmazonS3 _s3Client;
+        public BucketController()
+        {
+            var credentials = new BasicAWSCredentials("AKIAXVYKHZT4JY5YR3OQ", "evSoJosRddBWib0j4OieCqTGjowMka1bnIfADAIT");
+            _s3Client = new AmazonS3Client(credentials, Amazon.RegionEndpoint.USEast1);
+        }
+
+        [HttpGet("get-all-buckets")]
+        public async Task<IActionResult> GetAllBucketAsync()
+        {
+            var data = await _s3Client.ListBucketsAsync();
+            var buckets = data.Buckets.Select(b => { return b.BucketName; });
+            return Ok(buckets);
+        }
+
+        [HttpPost("create-buckets")]
+        public async Task<IActionResult> CreateBucketAsync(string bucketName)
+        {
+            var bucketExists = await _s3Client.DoesS3BucketExistAsync(bucketName);
+            if (bucketExists) return BadRequest($"Bucket {bucketName} already exists.");
+            await _s3Client.PutBucketAsync(bucketName);
+            return Ok($"Bucket {bucketName} created.");
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteBucketAsync(string bucketName)
+        {
+            await _s3Client.DeleteBucketAsync(bucketName);
+            return NoContent();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ImageManagerApp/Controllers/ImageController.cs
@@ -0,0 +1,69 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageManagerApp.Controllers
+{
+    public class S3ObjectDto
+    {
+        public string? Name { get; set; }
+        public string? PresignedUrl { get; set; }
+    }
+    public class ImageController : Controller
+    {
+        private readonly IAmazonS3 _s3Client;
+        public ImageController()
+        {
+            var credentials = new BasicAWSCredentials("AKIAXVYKHZT4JY5YR3OQ", "evSoJosRddBWib0j4OieCqTGjowMka1bnIfADAIT");
+            _s3Client = new AmazonS3Client(credentials, Amazon.RegionEndpoint.USEast1);
+        }
+
+
+        [HttpPost("upload-image")]
+        public async Task<IActionResult> UploadFileAsync(IFormFile file, string bucketName, string? prefix)
+        {
+            var bucketExists = await _s3Client.DoesS3BucketExistAsync(bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var request = new PutObjectRequest()
+            {
+                BucketName = bucketName,
+                Key = string.IsNullOrEmpty(prefix) ? file.FileName : $"{prefix?.TrimEnd('/')}/{file.FileName}",
+                InputStream = file.OpenReadStream()
+            };
+            request.Metadata.Add("Content-Type", file.ContentType);
+            await _s3Client.PutObjectAsync(request);
+            return Ok($"File {prefix}/{file.FileName} uploaded to S3 successfully!");
+        }
+        [HttpGet("get-all-images")]
+        public async Task<IActionResult> GetAllFilesAsync(string bucketName, string? prefix)
+        {
+            var bucketExists = await _s3Client.DoesS3BucketExistAsync(bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var request = new ListObjectsV2Request()
+            {
+                BucketName = bucketName,
+                Prefix = prefix
+            };
+            var result = await _s3Client.ListObjectsV2Async(request);
+            var s3Objects = result.S3Objects.Select(s =>
+            {
+                var urlRequest = new GetPreSignedUrlRequest()
+                {
+                    BucketName = bucketName,
+                    Key = s.Key,
+                    Expires = DateTime.UtcNow.AddMinutes(1)
+                };
+                return new S3ObjectDto()
+                {
+                    Name = s.Key.ToString(),
+                    PresignedUrl = _s3Client.GetPreSignedURL(urlRequest),
+                };
+            });
+            return Ok(s3Objects);
+        }
+
+    }
+
+   
+}
